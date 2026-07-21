223c063e6159a0823357f740dd5de8507fe60445
Aditieeeee <aditiguptareco@gmail.com>
2026-07-21 14:48:25 +0530

    add function

---
+++ lab1/hello.js
@@ -0,0 +1,25 @@
+function sayHello(name){
+    console.log(`welcome to ${name}`);
+}
+
+sayHello('aditi gupta');//function with argument
+
+//arrow function 
+const sayHi=(name)=>{
+     console.log(`welcome to ${name}`);
+};
+
+sayHi('meeeeee');
+
+function sum(a,b){
+    return a+b ;
+}
+
+// sum(3 , 5);
+
+const saysum=(a,b)=>a+b ;
+const saysum=(a,b)=>{
+    return a+b;
+}
+  
+  
